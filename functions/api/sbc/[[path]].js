751c05a31fa21b939da63494e1f6f86052395a13
nayanramam <nayanramam1@gmail.com>
2026-06-17 10:55:53 -0700

    jklknkl

--- functions/api/sbc/[[path]].js
+++
@@ -1,5 +0,0 @@
-import { handleApiRequest } from '../../../shared/sbc-api.js';
-
-export async function onRequest(context) {
-  return handleApiRequest(context.request, context.env);
-}
--- workers/sbc-api/src/index.js
+++
@@ -1,7 +0,0 @@
-import { handleApiRequest } from '../../../shared/sbc-api.js';
-
-export default {
-  async fetch(request, env) {
-    return handleApiRequest(request, env);
-  },
-};
